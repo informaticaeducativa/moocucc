44857560b71d23e009e915ceea347dde3545b0f6
David Vanegas <davidvanegas7@hotmail.com>
2016-01-07 11:37:43 -0500

    quitar dropdown temas

--- app/views/template.blade.php
+++ app/views/template.blade.php
@@ -59,6 +59,7 @@
        otro elemento que se pueda ocultar al minimizar la barra -->
   <div class="collapse navbar-collapse navbar-ex1-collapse navbar_back">
     <ul class="nav navbar-nav">
+<!--
      <li class="dropdown">
 		<a href="#" class="dropdown-toggle" data-toggle="dropdown">
 			Temas <b class="caret"></b>
@@ -71,6 +72,7 @@
 			<li><a href="#"></a></li>
 		</ul>
 	 </li>
+-->
     </ul>
  
     <form class="navbar-form navbar-left" role="search" action="{{ URL::route('index') }}" method="POST">
