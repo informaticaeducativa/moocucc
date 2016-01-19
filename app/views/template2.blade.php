6e4f76b030a0bf131f649466f3f9fcf67f4dd132
Juan Roa <jdroa92@gmail.com>
2016-01-19 11:32:06 -0500

    Fixing template2 scripts

--- app/views/template2.blade.php
+++ app/views/template2.blade.php
@@ -14,6 +14,99 @@ clase index.blade.php
 
 	<!-- js -->
 	<script src= "{{URL::to('js/Chart.js')}}"></script>
+
+	<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	ga('create', 'UA-71691218-1', 'auto');
+	ga('send', 'pageview');
+	</script>
+</head>
+
+<body class="body_div bg_blue_80">
+
+	<!--Header Start Here-->
+	<header>
+		<nav class="navbar navbar-default" role="navigation">
+			<!-- El logotipo y el icono que despliega el menú se agrupan
+			para mostrarlos mejor en los dispositivos móviles -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse"
+				data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Desplegar navegación</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="{{ URL::route('index') }}"><img src="{{URL::to('imagenes/logo.png')}}" width="30px"></a>
+			</div>
+
+			<!-- Agrupar los enlaces de navegación, los formularios y cualquier
+			otro elemento que se pueda ocultar al minimizar la barra -->
+			<div class="collapse navbar-collapse navbar-ex1-collapse navbar_back">
+				<ul class="nav navbar-nav">
+				</ul>
+
+				<ul class="nav navbar-nav navbar-right">
+					@if (Session::get('tipo_usuario') == "Administrador")
+					<li><a href="{{ URL::route('administrador')}}">Panel Administrador</a></li>
+					@endif
+					@if (Session::get('user') != "")
+					<li><a href="{{ URL::route('chat') }}">Chat</a></li>
+					<li><a href="{{ URL::route('mis-badges') }}">Mis Badges</a></li>
+					<li><a href="{{ URL::route('mis-cursos') }}">Mis Cursos</a></li>
+
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+							{{ Session::get('user') }}<b class="caret"></b>
+						</a>
+						<ul class="dropdown-menu">
+							<li><a href="{{ URL::route('usuario', Session::get('user_id') ) }}">Mi Perfil</a></li>
+							<li class="divider"></li>
+							<li><a href="{{ URL::route('logout')}}">Salir</a></li>
+						</ul>
+					</li>
+					@else
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
+						<ul id="login-dp" class="dropdown-menu">
+							<li>
+								<div class="row">
+									<div class="col-md-12">
+										<center>
+											Login via
+											<div class="social-buttons">
+												<a href="{{ URL::route('login-facebook') }}" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a><br><br>
+												<a href="{{ URL::route('login-twitter') }}" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a><br><br>
+												<a href="{{ URL::route('login-google') }}" class="btn btn-gp"><i class="fa fa-google"></i> Google +</a><br><br>
+											</div>
+										</center>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</li>
+					@endif
+				</ul>
+			</div>
+		</nav>
+	</header>
+
+	<!--
+		<div class="container" style="position:relative; top:-30px;">
+	-->
+	@yield('contenido', '')
+	<!--
+	</div>
+	-->
+
+	<!-- bootstrap - js -->
+	<script src= "http://code.jquery.com/jquery-2.2.0.min.js" ></script>
+	<script src= "//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+	<!-- bootstrap - js -->
+
 	<script>
 
 	$(document).ready(function() {
@@ -417,97 +510,6 @@ clase index.blade.php
 
 
 	</script>
-	<script>
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-	ga('create', 'UA-71691218-1', 'auto');
-	ga('send', 'pageview');
-	</script>
-</head>
-
-<body class="body_div bg_blue_80">
-
-	<!--Header Start Here-->
-	<header>
-		<nav class="navbar navbar-default" role="navigation">
-			<!-- El logotipo y el icono que despliega el menú se agrupan
-			para mostrarlos mejor en los dispositivos móviles -->
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse"
-				data-target=".navbar-ex1-collapse">
-					<span class="sr-only">Desplegar navegación</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="{{ URL::route('index') }}"><img src="{{URL::to('imagenes/logo.png')}}" width="30px"></a>
-			</div>
-
-			<!-- Agrupar los enlaces de navegación, los formularios y cualquier
-			otro elemento que se pueda ocultar al minimizar la barra -->
-			<div class="collapse navbar-collapse navbar-ex1-collapse navbar_back">
-				<ul class="nav navbar-nav">
-				</ul>
-
-				<ul class="nav navbar-nav navbar-right">
-					@if (Session::get('tipo_usuario') == "Administrador")
-					<li><a href="{{ URL::route('administrador')}}">Panel Administrador</a></li>
-					@endif
-					@if (Session::get('user') != "")
-					<li><a href="{{ URL::route('chat') }}">Chat</a></li>
-					<li><a href="{{ URL::route('mis-badges') }}">Mis Badges</a></li>
-					<li><a href="{{ URL::route('mis-cursos') }}">Mis Cursos</a></li>
-
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
-							{{ Session::get('user') }}<b class="caret"></b>
-						</a>
-						<ul class="dropdown-menu">
-							<li><a href="{{ URL::route('usuario', Session::get('user_id') ) }}">Mi Perfil</a></li>
-							<li class="divider"></li>
-							<li><a href="{{ URL::route('logout')}}">Salir</a></li>
-						</ul>
-					</li>
-					@else
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
-						<ul id="login-dp" class="dropdown-menu">
-							<li>
-								<div class="row">
-									<div class="col-md-12">
-										<center>
-											Login via
-											<div class="social-buttons">
-												<a href="{{ URL::route('login-facebook') }}" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a><br><br>
-												<a href="{{ URL::route('login-twitter') }}" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a><br><br>
-												<a href="{{ URL::route('login-google') }}" class="btn btn-gp"><i class="fa fa-google"></i> Google +</a><br><br>
-											</div>
-										</center>
-									</div>
-								</div>
-							</li>
-						</ul>
-					</li>
-					@endif
-				</ul>
-			</div>
-		</nav>
-	</header>
-
-	<!--
-		<div class="container" style="position:relative; top:-30px;">
-	-->
-	@yield('contenido', '')
-	<!--
-	</div>
-	-->
-
-	<!-- bootstrap - js -->
-	<script src= "http://code.jquery.com/jquery-2.2.0.min.js" ></script>
-	<script src= "//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
-	<!-- bootstrap - js -->
 
 </body>
 
